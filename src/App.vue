debcd98cab54380e6b22c362cac0cea3605c3907
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-03-09 10:32:49 +0000

    testing Velocity.js

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
 
  <v-app>
       <page-header/>
+      <transition
+       v-on:before-enter="beforeEnter"
+       v-on:enter="enter"
+ 
+      >
+       <event v-if="startAnimation"/>   
+      </transition>
       <v-content>
         <v-spacer></v-spacer>
         <v-container fluid>
@@ -16,20 +23,48 @@
 </template>
 
 <script>
+require('animate.css/animate.min.css');
 import HelloWorld from './components/HelloWorld';
 import PageHeader from './components/Header';
+import Event from './components/Events';
 
 
 export default {
   name: 'App',
+  data:() =>({
+    startAnimation: false
+  }),
   components: {
     HelloWorld,
-    PageHeader
+    PageHeader,
+    Event
   },
   data () {
     return {
       //
     }
+  },
+  methods: {
+        beforeEnter: function (el) {
+      el.style.opacity = 0    
+      Velocity(el, { translateX: '700px' }) 
+      },
+    enter: function (el, done) {
+      Velocity(el, { opacity: 1})
+      Velocity(el, { translateX: '0px'}, { duration: 800 })
+      Velocity(el, { complete: done })
+    },
+  
+  },
+  mounted: function(){
+this.startAnimation = true
   }
 }
 </script>
+
+
+<style lang="scss">
+.application--wrap{
+  min-height: inherit;
+}
+</style>
---
+++ src/components/Events.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app id="event">
+        <h1>This is an upcoming event</h1>
+        <p>Date</p>
+        <p>Place</p>
+
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+#event{
+background: #3949AB;
+color: white;
+font-size: 18px;
+height: 300px;
+padding: 30px;
+position: absolute;
+top: 200px;
+right: 0;
+}
+</style>
+
